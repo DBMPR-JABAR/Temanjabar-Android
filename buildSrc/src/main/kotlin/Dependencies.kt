c8f7a67743eb10bc395f65eef01e4012d7ea48a3
Ade Prianto <adeprianto21@gmail.com>
2022-02-21 10:06:57 +0700

    feat: setup map feature module

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -6,6 +6,23 @@ object DefaultConfig {
 }
 
 object Releases {
-    const val versionCode = 0
-    const val versionName = "0.0.0 (Alpha)"
+    const val versionCode = 1
+    const val versionName = "0.0.1 (Alpha)"
+}
+
+object Features {
+    const val map = ":features:map"
+}
+
+object  Libs {
+    const val gradleTools = "com.android.tools.build:gradle:7.1.1"
+    const val kotlinPlugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.10"
+
+    const val androidCore = "androidx.core:core-ktx:1.7.0"
+    const val appCompat = "androidx.appcompat:appcompat:1.4.1"
+    const val materialDesign = "com.google.android.material:material:1.5.0"
+
+    const val jUnit = "junit:junit:4.13.2"
+    const val androidJUnit = "androidx.test.ext:junit:1.1.3"
+    const val espresso = "androidx.test.espresso:espresso-core:3.4.0"
 }
