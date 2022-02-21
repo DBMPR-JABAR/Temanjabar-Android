60170fea4eca5e9e7543cda9c2658933b0770202
Ade Prianto <adeprianto21@gmail.com>
2022-02-21 09:19:35 +0700

    feat: add map feature module

---
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -0,0 +1,11 @@
+object DefaultConfig {
+    const val applicationId = "id.go.jabarprov.dbmpr.temanjabar"
+    const val minSdk = 21
+    const val targetSdk = 32
+    const val compileSdk = 32
+}
+
+object Releases {
+    const val versionCode = 0
+    const val versionName = "0.0.0 (Alpha)"
+}
