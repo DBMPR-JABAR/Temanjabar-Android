0ee1fab71f93615726833ac3c97bdc039a57dc09
Ade Prianto <adeprianto21@gmail.com>
2022-02-21 10:09:56 +0700

    refactor: refactor dependencies version in buildSrc

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -14,15 +14,28 @@ object Features {
     const val map = ":features:map"
 }
 
-object  Libs {
-    const val gradleTools = "com.android.tools.build:gradle:7.1.1"
-    const val kotlinPlugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.10"
+object Versions {
+    const val gradleTools = "7.1.1"
+    const val kotlinPlugin = "1.6.10"
 
-    const val androidCore = "androidx.core:core-ktx:1.7.0"
-    const val appCompat = "androidx.appcompat:appcompat:1.4.1"
-    const val materialDesign = "com.google.android.material:material:1.5.0"
+    const val androidCore = "1.7.0"
+    const val appCompat = "1.4.1"
+    const val materialDesign = "1.5.0"
 
-    const val jUnit = "junit:junit:4.13.2"
-    const val androidJUnit = "androidx.test.ext:junit:1.1.3"
-    const val espresso = "androidx.test.espresso:espresso-core:3.4.0"
+    const val jUnit = "4.13.2"
+    const val androidJUnit = "1.1.3"
+    const val espresso = "3.4.0"
+}
+
+object Libs {
+    const val gradleTools = "com.android.tools.build:gradle:${Versions.gradleTools}"
+    const val kotlinPlugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.kotlinPlugin}"
+
+    const val androidCore = "androidx.core:core-ktx:${Versions.androidCore}"
+    const val appCompat = "androidx.appcompat:appcompat:${Versions.appCompat}"
+    const val materialDesign = "com.google.android.material:material:${Versions.materialDesign}"
+
+    const val jUnit = "junit:junit:${Versions.jUnit}"
+    const val androidJUnit = "androidx.test.ext:junit:${Versions.androidJUnit}"
+    const val espresso = "androidx.test.espresso:espresso-core:${Versions.espresso}"
 }
