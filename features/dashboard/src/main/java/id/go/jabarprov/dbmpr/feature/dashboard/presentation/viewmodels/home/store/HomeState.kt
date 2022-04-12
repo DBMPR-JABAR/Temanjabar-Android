3a7fc4e9813faa2b0d2e867f64eed00da93f47eb
Ade Prianto <adeprianto21@gmail.com>
2022-04-12 08:05:13 +0700

    feat: add resource state class for wrapping data

--- features/dashboard/src/main/java/id/go/jabarprov/dbmpr/feature/dashboard/presentation/viewmodels/home/store/HomeState.kt
+++ features/dashboard/src/main/java/id/go/jabarprov/dbmpr/feature/dashboard/presentation/viewmodels/home/store/HomeState.kt
@@ -1,12 +1,9 @@
 package id.go.jabarprov.dbmpr.feature.dashboard.presentation.viewmodels.home.store
 
+import id.go.jabarprov.dbmpr.core_main.Resource
 import id.go.jabarprov.dbmpr.core_main.store.State
 import id.go.jabarprov.dbmpr.feature.dashboard.domain.entity.News
 
 data class HomeState(
-    val isFailed: Boolean = false,
-    val errorMessage: String = "",
-    val isSuccess: Boolean = false,
-    val isLoading: Boolean = false,
-    val listSliderNews: List<News>? = null
+    val listSlideNewsState: Resource<List<News>>
 ) : State
